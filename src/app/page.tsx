006b92c463cc8bcaa0c9b4f413a7bc8881301dbf
Jared Seto <jseto2018@gmail.com>
2025-09-04 10:02:52 -1000

    Implement Landing Page Design and update globals.css for button color

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,15 +3,27 @@ import { Col, Container, Image, Row } from 'react-bootstrap';
 /** The Home page. */
 const Home = () => (
   <main>
-    <Container id="landing-page" fluid className="py-3">
-      <Row className="align-middle text-center">
-        <Col xs={4}>
-          <Image src="next.svg" width="150px" alt="" />
+    <Container id="landing-page" fluid className="pt-5">
+      <Row className="align-items-center">
+        <Col xs={1} />
+        <Col xs={6} className="pt-5">
+          <h1>Effortlessly organize every kitchen space, from pantry to freezer</h1>
+          <p>Quickly see what you have, what you&apos;re running low on, and avoid double-buying.</p>
+          <Row className="align-items-center text-center">
+            <Col xs={5}>
+              <a href="/auth/signup" className="btn btn-primary btn-lg">
+                Sign Up
+              </a>
+            </Col>
+            <Col xs={3}>
+              <a href="/auth/signin" className="btn btn-primary btn-lg">
+                Sign In
+              </a>
+            </Col>
+          </Row>
         </Col>
-
-        <Col xs={8} className="d-flex flex-column justify-content-center">
-          <h1>Welcome to this template</h1>
-          <p>Now get to work and modify this app!</p>
+        <Col xs={4}>
+          <Image src="PantryWise_Logo.png" width="500px" />
         </Col>
       </Row>
     </Container>
