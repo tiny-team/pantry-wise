6eb0f2b60211cd8bc937f44c8f8dbb7f05b1e4f2
Jared Seto <jseto2018@gmail.com>
2025-09-04 12:15:11 -1000

    Added Images to bottom of landing page
    Images to show description of app capabilities.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,43 @@
-import { Col, Container, Image, Row } from 'react-bootstrap';
+import { Button, Col, Container, Image, Row } from 'react-bootstrap';
 
 /** The Home page. */
 const Home = () => (
   <main>
     <Container id="landing-page" fluid className="pt-5">
-      <Row className="align-items-center">
+      <Row className="d-flex justify-content-center align-items-center py-5">
         <Col xs={1} />
         <Col xs={6} className="pt-5">
-          <h1>Effortlessly organize every kitchen space, from pantry to freezer</h1>
-          <p>Quickly see what you have, what you&apos;re running low on, and avoid double-buying.</p>
-          <Row className="align-items-center text-center">
+          <h1>Track food. Save Time. Waste Less.</h1>
+          <h5>Effortlessly organize every kitchen space, from pantry to freezer</h5>
+          <Row className="align-items-center text-center pt-3">
             <Col xs={5}>
-              <a href="/auth/signup" className="btn btn-primary btn-lg">
+              <Button href="/auth/signup" className="btn btn-primary btn-lg">
                 Sign Up
-              </a>
+              </Button>
             </Col>
             <Col xs={3}>
-              <a href="/auth/signin" className="btn btn-secondary btn-lg">
+              <Button href="/auth/signin" className="btn btn-secondary btn-lg">
                 Sign In
-              </a>
+              </Button>
             </Col>
           </Row>
         </Col>
         <Col xs={4}>
-          <Image src="PantryWise_Logo.png" width="500px" />
+          <Image src="PantryWise_Logo.png" width="400px" />
+        </Col>
+      </Row>
+      <Row className="d-flex justify-content-center px-5 py-5 text-center">
+        <Col xs={3}>
+          <Image src="ShoppingCart.png" width="300px" />
+          <h2 className="pt-3">Smart Shopping</h2>
+        </Col>
+        <Col xs={3}>
+          <Image src="food-basket.png" width="300px" />
+          <h2 className="pt-3">Pantry Management</h2>
+        </Col>
+        <Col xs={3}>
+          <Image src="Calendar.png" width="300px" />
+          <h2 className="pt-3">Track Expiration</h2>
         </Col>
       </Row>
     </Container>
